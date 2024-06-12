e159fe26272203f50f1f80b8557ece75085f8ffe
Ruben Lucena <rubenlucena89@gmail.com>
2024-06-12 12:50:26 +0200

    Added FETCH Api

--- src/components/fetch/Fetch.jsx
+++ src/components/fetch/Fetch.jsx
@@ -6,12 +6,18 @@ const Fetch = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const headers = { 'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com', 'x-rapidapi-key': 'ec451c852bmsh5b9f324ff0aa41fp1b32e8jsn87d4f4065455' }
+    const headers = {
+      "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+      "x-rapidapi-key": "ec451c852bmsh5b9f324ff0aa41fp1b32e8jsn87d4f4065455", // Cada uno tiene su propia API, mirar en la página.
+    };
 
     const fetchGames = async () => {
       setLoading(true);
       try {
-        const response = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games?sort-by=popularity`, {mode: "cors", headers});
+        const response = await fetch(
+          `https://free-to-play-games-database.p.rapidapi.com/api/games?sort-by=popularity`,
+          { mode: "cors", headers }
+        );
         const data = await response.json();
         setGames(data);
         console.log(data);
@@ -22,7 +28,7 @@ const Fetch = () => {
       }
     };
     fetchGames();
-  }, []); 
+  }, []);
   if (loading) {
     return <p>Loading...</p>;
   }
@@ -34,7 +40,6 @@ const Fetch = () => {
       <ul>
         {games.map((game) => (
           <li key={game.id}>
-            
             <h2>{game.title}</h2>
             <img src={`${game.freetogame_profile_url}`} alt="" />
             <p>{game.short_description}</p>
