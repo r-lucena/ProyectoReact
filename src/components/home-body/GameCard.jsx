e268c37158ba662c6523ede118e11e26b6a8e72d
Ruben Lucena <rubenlucena89@gmail.com>
2024-06-28 05:49:12 +0200

    Added url to cards

--- src/components/home-body/GameCard.jsx
+++ src/components/home-body/GameCard.jsx
@@ -1,12 +1,12 @@
 import '../home-body/GameCard.css'
 const GameCard = ({ game }) => {
   return (
-    <div className="card">
-      <img src={game.thumbnail} alt={game.title} />
-      <h2>{game.title}</h2>
-      <p>{game.short_description}</p>
-      <p>Genre: {game.genre}</p>
-      <p>Platform: {game.platform}</p>
+    <div className="card" onClick={() => window.open(game.game_url)}>
+        <img src={game.thumbnail} alt={game.title} />
+        <h2>{game.title}</h2>
+        <p>{game.short_description}</p>
+        <p>Genre: {game.genre}</p>
+        <p>Platform: {game.platform}</p>
     </div>
   );
 };
