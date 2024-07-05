1a3aeaf1eeb1a7d0f03a5d0d4a4ee24386bf1dc9
Victor Milite <victormilite96@gmail.com>
2024-07-05 14:11:01 +0200

    we merged components-victor into development

--- src/components-victor/dropdown/BasicDropdown.jsx
+++ src/components-victor/dropdown/BasicDropdown.jsx
@@ -2,13 +2,13 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 
 function BasicDropdown({ btnName, objectsArray }) {
-    return (
-      <DropdownButton id="dropdown-basic-button" title={btnName}>
-        {objectsArray.map((object, index) => (
-            <Dropdown.Item href={object.href} key={index}>{object.item}</Dropdown.Item>
-        ))}
-      </DropdownButton>
-    );
-  }
-  
-  export default BasicDropdown;
+  return (
+    <DropdownButton className='dropdown-button' id="dropdown-basic-button" title={btnName}>
+      {objectsArray.map((object, index) => (
+        <Dropdown.Item href={object.href} key={index}>{object.item}</Dropdown.Item>
+      ))}
+    </DropdownButton>
+  );
+}
+
+export default BasicDropdown;
--- src/components-victor/navbar/Navbar.jsx
+++ src/components-victor/navbar/Navbar.jsx
@@ -19,25 +19,22 @@ const navbarDropdown = [
 ]
 
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+  // const toggleMenu = () => {
+  //   setIsOpen(!isOpen);
+  // };
 
   return (
     <nav className="navbar">
-      <div className="navbar-brand">
-        <a href="#"><img src="./src/assets/logos/logoSVG.svg" width={150} alt="logo" /></a>
-        <button className="navbar-toggle" onClick={toggleMenu}>
-          ☰
-        </button>
+      <div className='no-list-style'>
+        <div className="navbar-brand">
+
+          <a href="#"><img src="./src/assets/logos/logoSVG.svg" width={150} alt="logo" /></a>
+        </div>
+        <BasicDropdown btnName={"Links"} objectsArray={navbarDropdown} />
       </div>
-      <ul className={`navbar-menu ${isOpen ? 'is-open' : ''}`}>
-        <li >
-          <BasicDropdown btnName={"Links"} objectsArray={navbarDropdown} />
-        </li>
-      </ul>
+
       <ul className="other-links">
         <li className="navbar-item"><a href="/about">About</a></li>
         <li className="navbar-item"><a href="/services">Services</a></li>
