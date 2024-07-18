a166122775018675d64291b603565206e5e22e43
Victor Milite <victormilite96@gmail.com>
2024-07-18 14:38:31 +0200

    logic of GameSurveyCards is stated but not completed

--- src/components-victor/dropdown/BasicDropdown.jsx
+++ src/components-victor/dropdown/BasicDropdown.jsx
@@ -1,5 +1,7 @@
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import '../dropdown/BasicDropdown.css';
+
 
 function BasicDropdown({ btnName, objectsArray, handleOnClick }) {
   return (
