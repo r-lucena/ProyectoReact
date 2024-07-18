f537f69da32662046321abbb1f24161fadce9d90
Ruben Lucena <rubenlucena89@gmail.com>
2024-07-18 11:41:10 +0200

    Added css of cards contents and banner adjustment

--- src/components/home-body/GameCard.jsx
+++ src/components/home-body/GameCard.jsx
@@ -2,17 +2,23 @@ import '../home-body/GameCard.css'
 const GameCard = ({ game }) => {
   return (
     <div className="game-card-style">
-    <img 
-        src={game.thumbnail} 
-        alt={game.title} 
-        onClick={() => window.open(game.game_url)} 
-    />
-    <h2 onClick={() => window.open(game.game_url)}>
-        {game.title}
-    </h2>
-    <p>{game.short_description}</p>
-    <p>Genre: {game.genre}</p>
-    <p>Platform: {game.platform}</p>
+    <div className='card-img'>
+      <img
+          src={game.thumbnail}
+          alt={game.title}
+          onClick={() => window.open(game.game_url)}
+      />
+    </div>
+    <div className='card-title'>
+      <h2 onClick={() => window.open(game.game_url)}>
+          {game.title}
+      </h2>
+    </div>
+    <div className='card-texts'>
+      <p>{game.short_description}</p>
+      <p>Genre: {game.genre}</p>
+      <p>Platform: {game.platform}</p>
+    </div>
 </div>
 
   );
