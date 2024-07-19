e0eb673424e6caa7b95618dcaffe213131bff677
Ruben Lucena <rubenlucena89@gmail.com>
2024-07-19 12:27:51 +0200

    Added css style to gamesearch and edited the component

--- src/components/home-body/banner/Banner.jsx
+++ src/components/home-body/banner/Banner.jsx
@@ -1,19 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Banner.css'
-import bannerLarge from '../../../assets/banners/banner1900.png'
-import bannerMedium from '../../../assets/banners/banner1200.png'
-import bannerSmall from '../../../assets/banners/banner1080.png'
+
 
 
 const Banner = () => {
   return (
-    <div>
-    <picture className='banner'>
-      <source media="(max-width: 800px)" srcSet={bannerSmall} />
-      {/* <source media="(max-width: 1200px)" srcSet={bannerMedium} /> */}
-      <source media="(min-width: 1081px)" srcSet={bannerLarge} />
-      <img src={bannerLarge} alt="Banner" className="banner-image" />
-    </picture>
+    <div className='banner-container'>
+      <div className='banner'></div>
     </div>
 
   );
